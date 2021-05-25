f5dacd61b0c61b70d629fb9b5562c6da618512be
kaique caires <kaique.caires@yahoo.com>
2021-05-25 07:56:11 -0300

    refactor: move search to a component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Flex, Text, Input, Icon, HStack, Avatar, Box } from '@chakra-ui/react'
 import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 import { NotificationsNav } from './Header/NotificationsNav';
 import { Profile } from './Header/Profile'
+import { SearchBox } from './Header/SearchBox';
 
 export function Header() {
   return (
@@ -20,22 +21,7 @@ export function Header() {
         <Text as="span" ml="1" color="pink.500">.</Text>
       </Text>
 
-      <Flex
-        as="label"
-        flex="1"
-        py="4"
-        px="8"
-        ml="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        bg="gray.800"
-        borderRadius="full"
-      >
-        <Input color="gray.500" variant="unstyled" placeholder="Buscar na plataforma" _placeholder={{ color: "gray.400" }} />
-        <Icon as={RiSearchLine} fontSize="20" />
-      </Flex>
+      <SearchBox />
 
       <Flex
         align="center"
