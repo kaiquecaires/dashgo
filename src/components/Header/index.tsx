04e6213cb41fa49939617d5640ae403bae2c950c
kaique caires <kaique.caires@yahoo.com>
2021-05-26 07:24:24 -0300

    refactor: move Header to a folder

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import { Flex } from '@chakra-ui/react'
-import { Logo } from './Header/Logo';
-import { NotificationsNav } from './Header/NotificationsNav';
-import { Profile } from './Header/Profile'
-import { SearchBox } from './Header/SearchBox';
+import { Logo } from './Logo';
+import { NotificationsNav } from './NotificationsNav';
+import { Profile } from './Profile'
+import { SearchBox } from './SearchBox';
 
 export function Header() {
   return (
@@ -20,10 +20,7 @@ export function Header() {
 
       <SearchBox />
 
-      <Flex
-        align="center"
-        ml="auto"
-      >
+      <Flex align="center" ml="auto">
         <NotificationsNav />
         <Profile />
       </Flex>
