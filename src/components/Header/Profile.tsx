aefb4a92d2bed3c25af25121b927f3c2c18dd51d
kaique caires <kaique.caires@yahoo.com>
2021-05-31 18:51:00 -0300

    feat: make responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Kaique Caires</Text>
-        <Text color="gray.300" fontSize="small">kaique.caires@yahoo.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+         <Text>Kaique Caires</Text>
+         <Text color="gray.300" fontSize="small">kaique.caires@yahoo.com</Text>
+       </Box>
+      ) }
       <Avatar size="md" name="kaique Gonçaves" />
     </Flex>
   );
